f80e05fb322d7be83a341d9df19de8feddebedd3
KriegerTobias <krieger.tobias@live.de>
2021-02-20 17:32:30 +0100

    Remove existing web frontend

--- webfrontend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo Building mojo2600/pixelframe:build
-
-docker build --build-arg NODE_ENV="production" -t mojo2600/pixelframe:build .
-
-docker container create --name extract mojo2600/pixelframe:build
-#find ../data -mindepth 1 ! -regex '^.*homie.*' -delete
-docker container cp extract:/app/dist/. ./dist/
-docker container rm -f extract
--- webfrontend/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  productionSourceMap: process.env.NODE_ENV == 'production' ? false : true,
-  transpileDependencies: [
-    "vuetify"
-  ]
-}
