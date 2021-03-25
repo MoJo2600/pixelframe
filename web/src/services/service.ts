19002aa5077dffe3c82bd05f5cdf4c60bd3bee94
KriegerTobias <krieger.tobias@live.de>
2021-03-25 22:22:48 +0100

    Remove error on missing API base url environment variable

--- web/src/services/service.ts
+++ web/src/services/service.ts
@@ -5,13 +5,9 @@ export abstract class Service {
   protected readonly httpClient: AxiosInstance;
 
   constructor() {
-    if (!process.env.VUE_APP_API_BASE_URL) {
-      throw new Error(
-        '"VUE_APP_API_BASE_URL" must be provided in the environment'
-      );
-    }
-
     this.baseUrl = process.env.VUE_APP_API_BASE_URL || window.location.origin;
+
+    console.log(`API: ${this.baseUrl}`);
 
     this.httpClient = Axios.create({
       baseURL: `${this.baseUrl}/api`,
