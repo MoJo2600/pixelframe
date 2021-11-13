47ff64dca529cf597fcf1e9a6a615cbb0789b240
Christian Erhardt <mail@mojo2k.de>
2021-11-13 21:51:19 +0100

    added mklittlefs for windows

--- replace_fs.py
+++ replace_fs.py
@@ -1,3 +1,11 @@
 Import("env")
+from sys import platform
+
 print("Replace MKSPIFFSTOOL with mklittlefs")
-env.Replace (MKSPIFFSTOOL = "mklittlefs/mklittlefs")
+if platform == "linux" or platform == "linux2":
+    env.Replace (MKSPIFFSTOOL = "mklittlefs/mklittlefs")
+elif platform == "darwin":
+    # OS X
+    pass
+elif platform == "win32":
+    env.Replace (MKSPIFFSTOOL = "mklittlefs\mklittlefs.exe")
