2bb8cb7668282bc6835525d749bd927923288d3f
Christof Weigl <christof.weigl@continental-corporation.com>
2021-11-14 14:54:24 +0100

    added mklittlefs for darwin

--- replace_fs.py
+++ replace_fs.py
@@ -6,6 +6,6 @@ if platform == "linux" or platform == "linux2":
     env.Replace (MKSPIFFSTOOL = "mklittlefs/mklittlefs")
 elif platform == "darwin":
     # OS X
-    pass
+    env.Replace (MKSPIFFSTOOL = "mklittlefs/mklittlefs_darwin")
 elif platform == "win32":
     env.Replace (MKSPIFFSTOOL = "mklittlefs\mklittlefs.exe")
